35d24798169f00d4a515364d94f4e503be795959
Pete Wall <pete.wall@grafana.com>
2026-02-04 14:54:26 -0600

    More tests
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- internal/logging/compact_diff_logger_test.go
+++ internal/logging/compact_diff_logger_test.go
@@ -1,12 +1,12 @@
 package logging_test
 
 import (
-	"github.com/grafana/k8s-manifest-tail/internal/logging"
 	"testing"
 
 	"github.com/onsi/gomega"
 
 	"github.com/grafana/k8s-manifest-tail/internal/config"
+	"github.com/grafana/k8s-manifest-tail/internal/logging"
 	"github.com/grafana/k8s-manifest-tail/internal/manifest"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -27,4 +27,61 @@ func TestCompactDiffLoggerLogsChanges(t *testing.T) {
 	g.Expect(stub.records).To(gomega.HaveLen(1))
 	body := stub.records[0].Body().AsString()
 	g.Expect(body).To(gomega.ContainSubstring("Object created: Pod default/api"))
+}
+
+func TestCompactDiffLoggerLogsDeletion(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	stub := &StubLogger{}
+	logger := logging.NewDiffLogger(config.LoggingConfig{LogDiffs: config.LogDiffsCompact}, stub)
+	prev := &unstructured.Unstructured{}
+	prev.SetKind("Pod")
+	prev.SetNamespace("default")
+	prev.SetName("api")
+
+	logger.Log(&manifest.Diff{Previous: prev})
+
+	g.Expect(stub.records).To(gomega.HaveLen(1))
+	body := stub.records[0].Body().AsString()
+	g.Expect(body).To(gomega.ContainSubstring("Object deleted: Pod default/api"))
+}
+
+func TestCompactDiffLoggerLogsModification(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	stub := &StubLogger{}
+	logger := logging.NewDiffLogger(config.LoggingConfig{LogDiffs: config.LogDiffsCompact}, stub)
+	prev := &unstructured.Unstructured{}
+	prev.SetKind("Pod")
+	prev.SetNamespace("default")
+	prev.SetName("api")
+	current := &unstructured.Unstructured{}
+	current.SetKind("Pod")
+	current.SetNamespace("default")
+	current.SetName("api")
+
+	logger.Log(&manifest.Diff{Previous: prev, Current: current})
+
+	g.Expect(stub.records).To(gomega.HaveLen(1))
+	body := stub.records[0].Body().AsString()
+	g.Expect(body).To(gomega.ContainSubstring("Object modified: Pod default/api"))
+}
+
+func TestCompactDiffLoggerLogsClusterScopedCreation(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	stub := &StubLogger{}
+	logger := logging.NewDiffLogger(config.LoggingConfig{LogDiffs: config.LogDiffsCompact}, stub)
+	clusterRole := &unstructured.Unstructured{}
+	clusterRole.SetKind("ClusterRole")
+	clusterRole.SetName("my-cluster-role")
+
+	logger.Log(&manifest.Diff{Current: clusterRole})
+
+	g.Expect(stub.records).To(gomega.HaveLen(1))
+	body := stub.records[0].Body().AsString()
+	g.Expect(body).To(gomega.ContainSubstring("Object created: ClusterRole my-cluster-role"))
 }
